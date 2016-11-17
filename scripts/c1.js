adcea588b4cb5a5e342708721dd729bb572f4368
Stefan <stefanblazevski226@gmail.com>
2016-11-17 12:29:49 +0100

    C1

---
+++ scripts/c1.js
@@ -0,0 +1,91 @@
+// var ime = 'Bojan';
+// var vozrast = 34;
+// var visina = 182.4;
+// var vozachka_dozvola = true;
+
+// console.log('Zdravo Svetu!');
+
+// console.log(ime);
+// console.log(vozrast);
+// console.log(visina);
+// console.log(vozachka_dozvola);	
+
+// console.debug('debugging some verica.....');
+// console.error('My name is not' +ime);
+// console.info('this guy is ' + visina + ' tall');
+
+
+// var vozilo= 'peugeot';
+// var godina= 2014;
+// var proizvedeno= 2013;
+// var kmh= 50000.7;
+// var sostojba= 'vozna';
+// var BrojNaLica= 4;
+// var BrojNaStakla= 6;
+// var BrojNaOgledala= 3;
+// var KozhniSedishta= "true";
+// var ZimskaOprema="true";
+// var GoleminaNaBandazhi= 17;
+// var ShirinaNaGuma= 205;
+// var TipNaSvetlo= "halogen";
+// var kradeno= "true";
+// var krsheno="false";
+// var komandiNaVolan="true";
+// var vozenoPodDejstvo="epten";
+// var zgazeniChoeveci= 10;
+// var zgazeniRomi= 28.5;
+// var shvercaniDeca= 15;
+// var zaribano="false";
+
+// console.log('PEUGEOT SALOON');
+// console.log('vozilo: '+ vozilo);
+// console.log('godina: '+ godina);
+// console.log('proizvodstvo: '+ proizvedeno);
+// console.log('kilometrazha: ' + kmh);
+// console.log('sostojba: ' +sostojba);
+// console.log('broj na lica:' +BrojNaLica);
+// console.log('broj na stakla';BrojNaStakla);
+// console.log(BrojNaOgledala);
+// console.info(KozhniSedishta);
+// console.info(ZimskaOprema);
+
+// var a= 10;
+// var b= 4;
+// var c= 7;
+// var d= 10;
+
+// var e = a + b * c / d;
+
+// console.log(e);
+
+// var g = 10;
+// var h = 3;
+
+// console.log(g % h);
+
+// function zdravo(){
+// 	console.log('Zdravo Batka');
+// }
+
+// zdravo();
+
+// function pozdrav(ime){
+// 	console.log('Zdravo' + ime);
+// }
+
+// pozdrav(' Pero');
+// pozdrav(' janko');
+// pozdrav(' petko');
+
+
+
+function f2c(f){
+	console.log((f - 32) / 1.8);
+}
+
+f2c(20);
+
+function c2f(c){
+	console.log ((c * 1.8 + 32));
+} 
+c2f(15);
