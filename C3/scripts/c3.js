1067def2c99f75d0c8abc81b68df5dbaadad106e
Stefan <stefanblazevski226@gmail.com>
2016-12-01 12:52:53 +0100

    c3

---
+++ C3/scripts/c3.js
@@ -0,0 +1,80 @@
+// var parni_broevi = [2, 4, 6, 8, 10];
+// console.log(parni_broevi);
+
+// console.log(parni_broevi[2]);
+
+// var iminja =['Bojan', 'Janko', 'Petko', 'Stanko'];
+
+// console.log(iminja);
+// console.log(iminja[3]);
+
+// var nizi= [
+// 	['Pero', 'Janko', 'Stanko'],
+// 	[123, 345, 456],
+// 	[true, false, true],
+// 	[1.85, 5.34, 1.43]
+// ];
+
+// console.log(nizi);
+// console.log(nizi[0][2]);
+
+// console.log(nizi.length); // kolku elementi ima edna niza
+
+// var n = [];
+// n[10] = 'Skopje';
+// n[20] = 'Veles' ;
+// n[22] = 'Ohrid' ;
+
+// var test = [
+// 	{ime: 'Pero Perovski', ocena: 5},
+// 	{ime: 'Janko Jankovski', ocena: 4},	
+// 	{ime: 'Stanko Stankovski', ocena: 2},
+// 	{ime: 'Goran Goranovski', ocena: 3},
+// 	{ime: 'Ivan Ivanovski', ocena: 1}
+// ];
+
+// console.log(test[0].ime);
+// console.log(test[0].ocena);
+// console.log(test[1].ime);
+// console.log(test[2].ime);
+// console.log(test[3].ime);
+// console.log(test[4].ime);
+// console.log(test[4].ocena);
+
+
+// var funkcii ={
+// 	eden: function(){
+// 		console.log('funkcija br1');
+// 	}
+// 	dva: function(){
+// 		console.log('funkcija br2');
+// 	}
+// 	triL function(){
+// 		console.log('funckija br3');
+// 	}
+// }
+
+
+
+function cigari(br, cena, avtomobil){
+	
+		 dacia:
+			return(11000 * 61.5) / (br *365 / 19 * cena);
+				
+		skoda:
+			return(11000 * 61.5) / (br *365 / 19 * cena);
+		
+		 vw:
+			return(11000 * 61.5) / (br *365 / 19 * cena);
+		
+		 audi:
+			return(11000 * 61.5) / (br *365 / 19 * cena);
+		
+		 bmw:
+			return(11000 * 61.5) / (br *365 / 19 * cena);
+		
+	}
+}
+
+var broj_na_cigar = 38;
+var kutija_cena = 140;
